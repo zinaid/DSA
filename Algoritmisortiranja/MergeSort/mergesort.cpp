b1e124b85a030d30ee708b4fd2005d2494d6fbf3
Zinaid Kapić <zinaid_kapic@hotmail.com>
2023-05-20 14:41:56 +0200

    Modify merge sort

--- Algoritmisortiranja/MergeSort/mergesort.cpp
+++ Algoritmisortiranja/MergeSort/mergesort.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
 
-void merge(int arr[], int left, int mid, int right)
+void merge(int arr[], int left, int mid, int right, int size)
 {
     int i = left;
     int j = mid + 1;
-    int k = 0;
+    int k = left;
 
-    int temp[right - left + 1];
+    // Kreiranje privremenog niza
+    int temp[size];
 
     while (i <= mid && j <= right)
     {
@@ -24,7 +25,7 @@ void merge(int arr[], int left, int mid, int right)
             k++;
         }
     }
-
+    // Kopiranje ostatka elemenata lijevog podniza, ako ih ima
     while (i <= mid)
     {
         temp[k] = arr[i];
@@ -32,6 +33,7 @@ void merge(int arr[], int left, int mid, int right)
         k++;
     }
 
+    // Kopiranje ostatka elemenata desnog podniza, ako ih ima
     while (j <= right)
     {
         temp[k] = arr[j];
@@ -39,40 +41,46 @@ void merge(int arr[], int left, int mid, int right)
         k++;
     }
 
-    for (int p = 0; p < k; p++)
+    // kopiranje privremenog niza u originalni niz
+    for (int p = left; p <= right; p++)
     {
-        arr[left + p] = temp[p];
+        arr[p] = temp[p];
     }
 }
 
-void mergeSort(int arr[], int left, int right)
+void mergeSort(int arr[], int left, int right, int size)
 {
+    // ovaj uslov se nece ispuniti samo kad ostane jedan element
     if (left < right)
     {
+        // sredina
         int mid = (left + right) / 2;
 
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
+        // rekurzivni pozivi mergeSort funkcije na lijevi podniz i desni podniz
+        mergeSort(arr, left, mid, size);
+        mergeSort(arr, mid + 1, right, size);
 
-        merge(arr, left, mid, right);
+        // merge
+        merge(arr, left, mid, right, size);
     }
 }
 
 int main()
 {
-    int myarray[] = {2, 1, 5, 3, 4, 10, 1, 17};
-    int arr_size = sizeof(myarray) / sizeof(myarray[0]);
+    int myarray[] = {1, 2, 4, 3, 11, 19, 7, 4, 1};
+    int size = sizeof(myarray) / sizeof(myarray[0]);
 
     cout << "Prije sortiranja: " << endl;
-    for (int i = 0; i < arr_size; i++)
+    for (int i = 0; i < size; i++)
     {
         cout << myarray[i] << " ";
     }
     cout << endl;
-    mergeSort(myarray, 0, (arr_size - 1));
+
+    mergeSort(myarray, 0, (size - 1), size);
 
     cout << "Poslije sortiranja: " << endl;
-    for (int i = 0; i < arr_size; i++)
+    for (int i = 0; i < size; i++)
     {
         cout << myarray[i] << " ";
     }
